1a38d86324905195a1fbe43528bbd9a7a7eee2e2
jeyabaljb <jeyabalt36@gmail.com>
2022-03-26 23:33:31 +0530

    Hello

---
+++ GitLearn.java
@@ -0,0 +1,17 @@
+package com.Git;
+
+public class GitLearn {
+
+public static void main(String[] args) {
+	
+	int count=0;
+	for (int i = 1; i <= 100; i++) {
+		if(i%2==1)
+		{
+			count++;
+			
+		}
+	}
+	System.out.println(count);
+}
+}
